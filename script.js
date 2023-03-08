d838dc02af3d0fbf0e1104690c754684fd64554f
Brian Nicolas Pizarro <121734134+BNicDev@users.noreply.github.com>
2023-03-08 12:35:30 -0300

    function beta-1

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 var monthInput = document.querySelector('.month-value');
 var monthBills = document.querySelector('.month-bills');
+var totalIng = document.querySelector('.total-month');
+const infoContainer = document.querySelector('.info-container')
+const infoList = document.querySelector('.info-list')
+const btnNext = document.querySelector('.next');
 const btnCalc = document.querySelector('.calc-btn');
+const backBtn = document.querySelector('.back-btn') 
 
+btnNext.addEventListener('click', continuarFuncion);
 btnCalc.addEventListener('click', calcularmensuales);
+backBtn.addEventListener('click', backFuncion)
+
+function continuarFuncion(){
+    const isInfoListClosed = infoList.classList.remove('inactive');
+
+    if(isInfoListClosed){
+        infoList.classList.toggle('inactive')
+    }
+}
+
+function backFuncion(){
+    const isInfoListOpened = infoList.classList.contains('inactive');
+
+    if(isInfoListOpened){
+        infoList.classList.add('inactive')
+    }
+}
+
 
 function calcularmensuales(){
     var totalMonth =  monthInput.value + monthBills.value;
