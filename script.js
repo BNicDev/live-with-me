f1ee1989e5297b6037cb2a0e9fda7cf1d7e0e7c7
BNicDev <bnicdev@gmail.com>
2023-03-09 12:45:16 -0300

    function beta 2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var monthInput = document.querySelector('.month-value');
 var monthBills = document.querySelector('.month-bills');
 var totalIng = document.querySelector('.total-month');
+const resultContainer = document.querySelector('.result-container')
 const infoContainer = document.querySelector('.info-container')
 const infoList = document.querySelector('.info-list')
 const btnNext = document.querySelector('.next');
@@ -8,26 +9,22 @@ const btnCalc = document.querySelector('.calc-btn');
 const backBtn = document.querySelector('.back-btn') 
 
 btnNext.addEventListener('click', continuarFuncion);
-btnCalc.addEventListener('click', calcularmensuales);
-backBtn.addEventListener('click', backFuncion)
+//btnCalc.addEventListener('click', calcularmensuales);
+//backBtn.addEventListener('click', backFuncion)
 
 function continuarFuncion(){
-    const isInfoListClosed = infoList.classList.remove('inactive');
+    const isListClosed = infoList.classList.contains('inactive');
 
-    if(isInfoListClosed){
-        infoList.classList.toggle('inactive')
+    if(isListClosed){
+        infoList.classList.toggle('inactive');
+    }
+
+    if(totalIng.value == '2'){
+        infoContainer.innerHTML = "<label>ingrese el valor del ingreso numero 1</label> <br> <input> <br> <label>ingrese el valor del ingreso numero 2</label> <br> <input>";
+        resultContainer.innerHTML = "<h2 class = 'tittle'>La cantidad de ingresos mensual es</h2> <br> "+totalIng.value+"" 
     }
 }
 
-function backFuncion(){
-    const isInfoListOpened = infoList.classList.contains('inactive');
-
-    if(isInfoListOpened){
-        infoList.classList.add('inactive')
-    }
-}
 
 
-function calcularmensuales(){
-    var totalMonth =  monthInput.value + monthBills.value;
-}
+
